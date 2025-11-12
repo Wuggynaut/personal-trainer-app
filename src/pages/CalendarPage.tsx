199813eb95bd6f4f0efca8f837426b1359c92751
Wuggynaut <arimatti.toivonen@gmail.com>
2025-11-12 11:28:01 +0200

    collated calendar components

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,15 +1,46 @@
-
-import { TrainingCalendar } from "../components/TrainingCalendar";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import type { Training } from "../types";
+import './TrainingCalendar.css';
 
 type CalendarPageProps = {
     trainings: Training[];
 }
 
 export function CalendarPage({ trainings }: CalendarPageProps) {
+    const events = trainings.map(training => ({
+        title: `${training.activity} - ${training.customer.lastname}`,
+        start: training.date.toISOString(),
+        end: training.date.add(training.duration, 'minute').toISOString()
+    }));
+
     return (
-        <TrainingCalendar
-            trainings={trainings}
+        <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin]}
+            initialView="dayGridMonth"
+            headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            slotLabelFormat={{
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
+            }}
+            eventTimeFormat={{
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
+            }}
+            displayEventTime={false}
+            slotMinTime="06:00:00"
+            slotMaxTime="22:00:00"
+            events={events}
+            height="auto"
+            eventDisplay="block"
+            dayMaxEvents={true}
         />
     )
 }
